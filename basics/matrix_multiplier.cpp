27445f1c78e02f61493cde30810be845f6017369
Abhinav Tiwari <tiwariabhinav036@gmail.com>
2026-07-12 17:22:14 +0530

    Added a program to find multiplication of matrix.

--- basics/matrix_multiplier.cpp
+++ basics/matrix_multiplier.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int A[2][2], B[2][2], C[2][2];
 
-    // Input matrix A
     cout << "Enter elements of 2x2 Matrix A:\n";
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
@@ -12,7 +11,6 @@ int main() {
         }
     }
 
-    // Input matrix B
     cout << "Enter elements of 2x2 Matrix B:\n";
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
