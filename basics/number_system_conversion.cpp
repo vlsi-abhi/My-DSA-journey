c3988b35e87fc877ce5962dbffab0e167fd9d2aa
Abhinav Tiwari <tiwariabhinav036@gmail.com>
2026-07-17 11:38:08 +0530

    Added a number system convertor.

---
+++ basics/number_system_conversion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bitset>   // for binary representation
+#include <iomanip>  // for hex formatting
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    // Binary using bitset (32 bits)
+    cout << "Binary: " << bitset<32>(n) << endl;
+
+    // Octal using stream manipulators
+    cout << "Octal: " << oct << n << endl;
+
+    // Hexadecimal using stream manipulators
+    cout << "Hexadecimal: " << hex << uppercase << n << endl;
+
+    return 0;
+}
